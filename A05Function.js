dff89e8e83256198afe2debd45a0cffba479c608
Anuj Jha <anujjha2106@gmail.com>
2024-04-27 02:24:44 +0530

    function & arrow function

--- A05Function.js
+++ A05Function.js
@@ -10,3 +10,30 @@ console.log(`Sum of ${a} and ${b} is ${sum}`)  // template literals
 
 //----------------------------------------------------------------
 
+function greeting(name){
+    console.log("Helooz", name);
+}
+greeting();
+greeting("Anuj");
+
+
+//----------------------------------------------------------------
+
+// Arrow Function: use for small piece of code 
+
+const arrowSum = (a, b)=>{
+    console.log(a+b);
+};
+
+const arrowMult = (a, b)=>{
+    return a*b;
+};
+
+console.log(arrowSum);
+arrowSum(5,10);
+
+console.log(arrowMult(5,8));
+
+let helloFun = (name) => console.log("Hello!!", name);
+helloFun("Anuj");
+
