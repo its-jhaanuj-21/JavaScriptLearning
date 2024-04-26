43a17c63852bf483118aca65c7b8137f644ed541
Anuj Jha <anujjha2106@gmail.com>
2024-04-27 01:50:51 +0530

    Function

---
+++ A05Function.js
@@ -0,0 +1,12 @@
+function sum(a, b){
+    return a+b;
+}
+
+a = 8, b = 9
+
+sum  = sum(a,b)
+console.log("Sum of",a,"and", b, "is", sum)
+console.log(`Sum of ${a} and ${b} is ${sum}`)  // template literals 
+
+//----------------------------------------------------------------
+
