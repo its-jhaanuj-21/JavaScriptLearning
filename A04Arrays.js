d06724000089d1206ad95dae0b855b27a2ec9de2
Anuj Jha <anujjha2106@gmail.com>
2024-04-27 01:39:44 +0530

    Arrays

--- A04Arrays.js
+++ A04Arrays.js
@@ -93,7 +93,7 @@ food.push("Cisco", "Accenture");
 console.log("After Push(): ");
 console.log(food);
 
-// pop()  : delete from end & return 
+// pop()  : delete from end in original array & return 
 console.log("");
 console.log("Before pop(): ");
 console.log(food);
@@ -104,9 +104,87 @@ console.log(food);
 console.log("Popped Item: ", deletedItem);
 
 // toString() : converts array to string 
+console.log("toString()")
 console.log(food.toString())
 
-// conact()
+// conact() : concat and return new array: 
+let marvel_heroes = ["Thor", "Spider-Man", "Iron-Man"];
+let dc_heroes = ["Super-Man", "Bat-Man"];
+let indian_heroes = ["Shaktiman", "Krrish"]
+let heroes2 = marvel_heroes.concat(dc_heroes, indian_heroes);
+console.log("After .concat():")
+console.log(heroes2)
+
+// unshift() : Add at start of array 
+console.log("Array Before unshift():")
+console.log(marvel_heroes)
+marvel_heroes.unshift("Ant-Man");
+console.log("Array After unshift():")
+console.log(marvel_heroes)
+
+// shift() : delete form start & return 
+console.log("Array Before shift():")
+console.log(marvel_heroes)
+let shift_val = marvel_heroes.shift();
+console.log("Array After shift():")
+console.log(marvel_heroes)
+console.log("shifted value : ", shift_val);
+
+// slice() : return a piece of the array
+console.log("Slice: ")
+let numArray = [1,2,3,4,5,6,7,8,9];
+let slicedArray1 = numArray.slice(2, 6)
+console.log(slicedArray1);
+ console.log(numArray.slice(4))
+
+// splice() : change original array (add, remove, replace)
+console.log("Splice: ")
+let numArray2 = [1,2, 3, 4, 5, 6]
+numArray2.splice(3) // delete from 3 to end 
+console.log(numArray2) 
+
+let numArray3 = [1,2,3,4,5,6,7,8,9];
+numArray3.splice(2, 3) // delete 3 values strating from index 2
+console.log(numArray3) 
+
+let numArray4 = [1,2,3,4,5,6,7,8,9]
+numArray4.splice(2,4,  101, 102, 103, 104, 105)  // delete 4 values from 2 and add as many as values at between vacant space
+console.log(numArray4)
+
+// it can also be in different way: 
+// Add Element
+let numbArr = [1,2,3,4];
+numbArr.splice(2, 0, 2.1, 2.2, 2.5, 2.6, 2.9 )
+console.log(numbArr)
+
+// Delete Element
+numbArr.splice(7, 1)
+console.log(numbArr)
+
+// replace element 
+numbArr.splice(7, 1, 10)
+console.log(numbArr)
+
+
+//----------------------------------------------------------------------------
+
+// Question 1 : Create an array  
+let comp = ["Bloomberg", "Microsoft", "Uber", "Google", "IBM", "Netflix"]
+console.log(comp)
+
+comp.shift()
+console.log(comp)
+
+comp.splice(0,1)
+console.log(comp)
+
+comp.splice(0,1, "Ola")
+console.log(comp)
+
+comp.push("Amazon")
+console.log(comp)
+
+
 
 
 
