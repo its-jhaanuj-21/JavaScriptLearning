d8080d2eae9be740cb13bc5f865a61a54e11547b
Anuj Jha <anujjha2106@gmail.com>
2024-04-29 22:47:56 +0530

    new commit

--- A06DOM.js
+++ A06DOM.js
@@ -44,4 +44,55 @@ console.dir(headings)
 let paras = document.getElementsByTagName("p")
 console.log(paras)
 
-// selecting elements by 
+
+
+// selecting elements by QuerySelector: 
+// select anything => id, class, tag
+
+let firstEl = document.querySelector("p"); // 1st  p
+console.dir(firstEl);
+
+let allEL = document.querySelectorAll("P");
+console.log(allEL);   // return NodeList => list of all nodes 
+
+let allClass = document.querySelectorAll(".heading")
+console.dir(allClass)
+
+let idEl = document.querySelector("#btn");
+console.dir(idEl);
+
+
+//------------------------------------------------------------
+// Properties of element : 
+    // can set and get the values of element by these properties 
+
+let elem1 = document.querySelector("#btn");
+console.dir(elem1.tagName)   // tagName
+
+let elem2 = document.getElementById("heading");
+console.dir(elem2.innerText)  // innerText
+
+let divEl = document.querySelector("div");
+console.dir(divEl.innerText) 
+
+
+console.dir(divEl.innerHTML)  // innerHTML
+
+divEl.innerText = "ABCD";
+divEl.innerHTML = "<div> Inner Div </div>";
+
+let heading1 = document.querySelector("h1");
+console.dir(heading1.innerText)
+heading1.innerText = "New Heading"
+console.dir(heading1.innerText)
+heading1.innerHTML = "<i> New Heading </i>"
+
+
+let h2 = document.querySelector("h2");
+console.dir(h2.innerText); // it will not print coz h2 is hidden 
+console.dir(h2.innerHTML)
+console.dir(h2.textContent)  // it will show hidden text as well 
+
+
+
+
