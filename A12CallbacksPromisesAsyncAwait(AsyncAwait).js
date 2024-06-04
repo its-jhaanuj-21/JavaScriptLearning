11ac1238e76a028bc1a9f46d9baf024046cc2359
Anuj Jha <anujjha2106@gmail.com>
2024-06-04 18:15:15 +0530

    async Await

--- A12CallbacksPromisesAsyncAwait(AsyncAwait).js
+++ A12CallbacksPromisesAsyncAwait(AsyncAwait).js
@@ -12,3 +12,26 @@ async function getWeatherData(){
     await api();  // 2nd
     await api();  // 3rd
 }
+
+
+//--------------------------------------------------------
+
+function getData(dataId){
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            console.log("Data: ", dataId);
+            resolve("success");
+        }, 2000);
+    });
+};
+
+async function getAllData(){
+    console.log("Getting data 1........");
+    await getData(1);
+    console.log("Getting data 2........");
+    await getData(2);
+    console.log("Getting data 3........");
+    await getData(3);
+    console.log("Getting data 4........");
+    await getData(4);
+}
