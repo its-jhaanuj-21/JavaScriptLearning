8a81a4ba8965f773c7241bb631c00a56a7512d37
Anuj Jha <anujjha2106@gmail.com>
2024-06-04 16:47:59 +0530

    Promise Chaining

--- A11CallbacksPromisesAsyncAwait.js
+++ A11CallbacksPromisesAsyncAwait.js
@@ -4,15 +4,15 @@
  *  It is a solution to callback Hell.
  *
  *  Three state -> pending, fullfilled, rejected3
- * 
+ *
  *  Pending : the result is undefined
  *  Resolved : the result is a value (fulfilled) ->  resolve(result)
  *  Rejected : the result is an error object    ->   reject(error)
  *
  * Generally Promises provided by API and we use it
- * 
- * 
- * 
+ *
+ *
+ *
  *
  */
 
@@ -42,7 +42,7 @@ let getData = (dataId, getNextdata) => {
       if (getNextdata) {
         getNextdata();
       }
-    }, 5000);
+    }, 2000);
   });
 };
 
@@ -51,41 +51,143 @@ let getData = (dataId, getNextdata) => {
 let getData2 = (dataId, getNextdata) => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-    //   console.log("data: ", dataId);
+      //   console.log("data: ", dataId);
       reject("Some Error Occured!!!");
       if (getNextdata) {
         getNextdata();
       }
-    }, 5000);
+    }, 2000);
   });
 };
 
-
 // /-------------------------------------------------------------------
 
-// using promises 
+// using promises
 
 /**
  * promise.than ((res) => {.......})    [if fulfilled]
- * 
+ *
  * promise.catch((err) => {.......})    [if reject]
  */
 
-const getPromise = () =>{
-    return new Promise((resolve, reject) => {
-        console.log("This is Promise");
-        resolve("Success!!");
-        // reject("Some Error has occured!!");
-    });
+const getPromise = () => {
+  return new Promise((resolve, reject) => {
+    console.log("This is Promise");
+    resolve("Success!!");
+    // reject("Some Error has occured!!");
+  });
 };
 
 let pro = getPromise();
 
-pro.then((res)=>{
-    console.log("Promise id fulfilled!!  |  ", res);
+pro.then((res) => {
+  console.log("Promise id fulfilled!!  |  ", res);
 });
 
-pro.catch((res)=>{
-    console.log("Promise is rejected!!  |", res );
-})
+pro.catch((res) => {
+  console.log("Promise is rejected!!  |", res);
+});
 
+// /-------------------------------------------------------------------
+
+// Promise Chaining
+
+function asyncFunc1() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Data 1");
+      resolve("Success");
+    }, 4000);
+  });
+}
+
+function asyncFunc2() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Data 2");
+      resolve("Success");
+    }, 4000);
+  });
+}
+
+console.log("Fetching Data 1........");
+let p1 = asyncFunc1();
+p1.then((res) => {
+  console.log(res);
+});
+
+console.log("Fetching Data 2........");
+let p2 = asyncFunc2();
+p1.then((res) => {
+  console.log(res);
+});
+// these both are happening simultaneously
+
+//----------------------------------------------------------
+// Now we will do promise chaining
+
+function asyncFunc3() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Data 3");
+      resolve("Success");
+    }, 5000);
+  });
+}
+function asyncFunc4() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Data 4");
+      resolve("Success");
+    }, 5000);
+  });
+}
+
+// console.log("Fetching data 3...........");
+// let p3 = asyncFunc3();
+// p3.then((res)=>{
+//     console.log(res);
+//     console.log("Fetching data 4...........");
+//     let p4 = asyncFunc4();
+//     p4.then((res)=>{
+//         console.log(res);
+//     })
+// })
+
+// we can shorten the chaining withou creating a varible
+
+console.log("Fetching data 3....");
+asyncFunc3().then((res) => {
+  console.log(res);
+  console.log("Fetching data 4....");
+  asyncFunc4().then((res) => {
+    console.log(res);
+  });
+});
+
+
+// /--------------------------------------------------------------
+// Another and actual way to promise chaining:
+
+function getData3(dataId) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Data: ", dataId);
+      resolve("Sucess");
+    }, 6000);
+  });
+}
+
+getData3(1000)
+  .then((res) => {
+    return getData3(2000);
+  })
+  .then((res) => {
+    return getData3(3000);
+  })
+  .then((res) => {
+    return getData3(4000);
+  })
+  .then((res) => {
+    console.log(res);
+  });
