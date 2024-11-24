9e17d1ef00f0bfe707003ad721c513935cb38b17
Anuj Jha <anujjha2106@gmail.com>
2024-11-25 02:42:40 +0530

    new Commit

--- A11CallbacksPromisesAsyncAwait(Promises).js
+++ A11CallbacksPromisesAsyncAwait(Promises).js
@@ -3,7 +3,7 @@
  *  Promise is for "eventual" completion of task. It is an object in JS.
  *  It is a solution to callback Hell.
  *
- *  Three state -> pending, fullfilled, rejected3
+ *  Three state -> pending, fullfilled, rejected
  *
  *  Pending : the result is undefined
  *  Resolved : the result is a value (fulfilled) ->  resolve(result)
@@ -16,49 +16,49 @@
  *
  */
 
-let promise = new Promise((resolve, reject) => {
-  console.log("I'm a promise");
-});
+// let promise = new Promise((resolve, reject) => {
+//   console.log("I'm a promise");
+// });
 
-let promise2 = new Promise((resolve, reject) => {
-  console.log("I'm Promise 2");
-  resolve("Successful");
-});
+// let promise2 = new Promise((resolve, reject) => {
+//   console.log("I'm Promise 2");
+//   resolve("Successful");
+// });
 
-let promise3 = new Promise((resolve, reject) => {
-  console.log("I'm Promise 3");
-  reject("Some Error Occurred");
-});
+// let promise3 = new Promise((resolve, reject) => {
+//   console.log("I'm Promise 3");
+//   reject("Some Error Occurred");
+// });
 
 //--------------------------------------------------
 
 // Creating a fulfilled promise
 
-let getData = (dataId, getNextdata) => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      console.log("data: ", dataId);
-      resolve("Success");
-      if (getNextdata) {
-        getNextdata();
-      }
-    }, 2000);
-  });
-};
+// let getData = (dataId, getNextdata) => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       console.log("data: ", dataId);
+//       resolve("Success");
+//       if (getNextdata) {
+//         getNextdata();
+//       }
+//     }, 2000);
+//   });
+// };
 
 // creating a rejected Promise
 
-let getData2 = (dataId, getNextdata) => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      //   console.log("data: ", dataId);
-      reject("Some Error Occured!!!");
-      if (getNextdata) {
-        getNextdata();
-      }
-    }, 2000);
-  });
-};
+// let getData2 = (dataId, getNextdata) => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       //   console.log("data: ", dataId);
+//       reject("Some Error Occured!!!");
+//       if (getNextdata) {
+//         getNextdata();
+//       }
+//     }, 2000);
+//   });
+// };
 
 // /-------------------------------------------------------------------
 
@@ -70,78 +70,80 @@ let getData2 = (dataId, getNextdata) => {
  * promise.catch((err) => {.......})    [if reject]
  */
 
-const getPromise = () => {
-  return new Promise((resolve, reject) => {
-    console.log("This is Promise");
-    resolve("Success!!");
-    // reject("Some Error has occured!!");
-  });
-};
+// const getPromise = () => {
+//   return new Promise((resolve, reject) => {
+//     console.log("This is Promise");
+//     resolve("Success!!");
+//     // reject("Some Error has occured!!");
+//   });
+// };
 
-let pro = getPromise();
+// let pro = getPromise();
 
-pro.then((res) => {
-  console.log("Promise id fulfilled!!  |  ", res);
-});
+// pro.then((res) => {
+//   console.log("Promise id fulfilled!!  |  ", res);
+// });
 
-pro.catch((res) => {
-  console.log("Promise is rejected!!  |", res);
-});
+// pro.catch((res) => {
+//   console.log("Promise is rejected!!  |", res);
+// });
 
 // /-------------------------------------------------------------------
 
 // Promise Chaining
 
-function asyncFunc1() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      console.log("Data 1");
-      resolve("Success");
-    }, 4000);
-  });
-}
+// function asyncFunc1() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       console.log("Data 1");
+//       resolve("Success");
+//     }, 4000);
+//   });
+// }
 
-function asyncFunc2() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      console.log("Data 2");
-      resolve("Success");
-    }, 4000);
-  });
-}
+// function asyncFunc2() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       console.log("Data 2");
+//       resolve("Success");
+//     }, 4000);
+//   });
+// }
 
-console.log("Fetching Data 1........");
-let p1 = asyncFunc1();
-p1.then((res) => {
-  console.log(res);
-});
+// console.log("Fetching Data 1........");
+// let p1 = asyncFunc1();
+// p1.then((res) => {
+//   console.log(res);
+// });
 
-console.log("Fetching Data 2........");
-let p2 = asyncFunc2();
-p1.then((res) => {
-  console.log(res);
-});
+// console.log("Fetching Data 2........");
+// let p2 = asyncFunc2();
+// p1.then((res) => {
+//   console.log(res);
+// });
+
+
 // these both are happening simultaneously
 
 //----------------------------------------------------------
 // Now we will do promise chaining
 
-function asyncFunc3() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      console.log("Data 3");
-      resolve("Success");
-    }, 5000);
-  });
-}
-function asyncFunc4() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      console.log("Data 4");
-      resolve("Success");
-    }, 5000);
-  });
-}
+// function asyncFunc3() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       console.log("Data 3");
+//       resolve("Success");
+//     }, 5000);
+//   });
+// }
+// function asyncFunc4() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       console.log("Data 4");
+//       resolve("Success");
+//     }, 5000);
+//   });
+// }
 
 // console.log("Fetching data 3...........");
 // let p3 = asyncFunc3();
@@ -156,14 +158,14 @@ function asyncFunc4() {
 
 // we can shorten the chaining withou creating a varible
 
-console.log("Fetching data 3....");
-asyncFunc3().then((res) => {
-  console.log(res);
-  console.log("Fetching data 4....");
-  asyncFunc4().then((res) => {
-    console.log(res);
-  });
-});
+// console.log("Fetching data 3....");
+// asyncFunc3().then((res) => {
+//   console.log(res);
+//   console.log("Fetching data 4....");
+//   asyncFunc4().then((res) => {
+//     console.log(res);
+//   });
+// });
 
 
 // /--------------------------------------------------------------
